c0e06d00ee472b9e74f9520c3e9a39679fdf2f3d
varan555 <36423268+varan555@users.noreply.github.com>
2020-05-11 19:04:37 +0300

    Add files via upload

---
+++ java-project/student-order/src/main/java/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -0,0 +1,21 @@
+package edu.javacourse.studentorder;
+
+import edu.javacourse.studentorder.dao.DictionaryDaoImpl;
+import edu.javacourse.studentorder.dao.StudentOrderDao;
+import edu.javacourse.studentorder.dao.StudentOrderDaoImpl;
+import edu.javacourse.studentorder.domain.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class SaveStudentOrder
+{
+
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer = 199;
+        System.out.println("saveStudentOrder");
+
+        return answer;
+    }
+
+}
---
+++ java-project/student-order/src/main/java/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -0,0 +1,59 @@
+package edu.javacourse.studentorder.validator;
+
+import edu.javacourse.studentorder.domain.Child;
+import edu.javacourse.studentorder.domain.Person;
+import edu.javacourse.studentorder.domain.register.AnswerCityRegister;
+import edu.javacourse.studentorder.domain.register.AnswerCityRegisterItem;
+import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
+import edu.javacourse.studentorder.domain.StudentOrder;
+import edu.javacourse.studentorder.exception.CityRegisterException;
+import edu.javacourse.studentorder.validator.register.CityRegisterChecker;
+import edu.javacourse.studentorder.validator.register.RealCityRegisterChecker;
+
+public class CityRegisterValidator
+{
+    public static final String IN_CODE = "NO_GRN";
+
+    private CityRegisterChecker personChecker;
+
+    public CityRegisterValidator() {
+        personChecker = new RealCityRegisterChecker();
+    }
+
+    public AnswerCityRegister checkCityRegister(StudentOrder so) {
+        AnswerCityRegister ans = new AnswerCityRegister();
+
+        ans.addItem(checkPerson(so.getHusband()));
+        ans.addItem(checkPerson(so.getWife()));
+        for (Child child : so.getChildren()) {
+            ans.addItem(checkPerson(child));
+        }
+
+        return ans;
+    }
+
+    private AnswerCityRegisterItem checkPerson(Person person) {
+        AnswerCityRegisterItem.CityStatus status = null;
+        AnswerCityRegisterItem.CityError error = null;
+
+        try {
+            CityRegisterResponse tmp = personChecker.checkPerson(person);
+            status = tmp.isRegistered() ?
+                    AnswerCityRegisterItem.CityStatus.YES :
+                    AnswerCityRegisterItem.CityStatus.NO;
+        } catch (CityRegisterException ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(ex.getCode(), ex.getMessage());
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
+        }
+
+        AnswerCityRegisterItem ans =
+                new AnswerCityRegisterItem(status, person, error);
+
+        return ans;
+    }
+}
